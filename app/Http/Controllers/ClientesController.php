00e12a89424798f1da3fa875df71d56963aaf6a5
Gustavo <gustavosnik95@gmail.com>
2019-01-29 16:02:08 -0200

    second commit

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cliente;
+
+class ClientesController extends Controller
+{
+   	public function create(Request $request){
+		$cliente = new Cliente;
+		$cliente->nome = $request->nome;
+		$cliente->telefone = $request->telefone;
+		$cliente->dataDeNascimento = $request->dataDeNascimento;
+		$cliente->cpf = $request->cpf;
+		$cliente->save();
+		return response()->json([$cliente]);
+	}
+}
+
+
